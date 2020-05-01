4e23cd84466cab76f0ca8938f628d1e8e1d57fc8
Nikolaus Schauersberger <nikolaus.schauersberger@gmail.com>
2020-05-01 12:58:21 +0200

    Add buildSrc and remove database

---
+++ buildSrc/src/main/java/Dependencies.kt
@@ -0,0 +1,22 @@
+const val kotlin = "1.2.71"
+
+object Kotlin {
+    const val kotlinStandardLibrary = "org.jetbrains.kotlin:kotlin-stdlib-jdk7:${kotlin}"
+}
+
+object Androidx {
+    const val core =  "androidx.core:core-ktx:1.2.0"
+    const val appcompat = "androidx.appcompat:appcompat:1.1.0"
+    const val constraintLayout = "androidx.constraintlayout:constraintlayout:1.1.3"
+
+    const val navigation = "androidx.navigation:navigation-ui:2.2.1"
+    const val lifecycleExtensions = "androidx.lifecycle:lifecycle-extensions:2.0.0"
+    const val navigationFragment = "androidx.navigation:navigation-fragment-ktx:2.2.1"
+    const val navigationUi = "androidx.navigation:navigation-ui-ktx:2.2.1"
+    const val legacySupport = "androidx.legacy:legacy-support-v4:1.0.0"
+    const val lifecycleViewmodel = "androidx.lifecycle:lifecycle-viewmodel-ktx:2.0.0"
+}
+
+object Material {
+    const val main = "com.google.android.material:material:1.1.0"
+}
